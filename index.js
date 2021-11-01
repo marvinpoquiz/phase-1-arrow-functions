e5401e2f31c0d756130e3fefac945113e8124ae2
Marvin <marvinepoquiz@gmail.com>
2021-11-01 12:26:35 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (x = 2000, y = 100) => {
+    return x / y
+  }
+  
+  divide();
+  
+  const square = (x = 2) =>  {
+    return x * x
+  }
+  
+  square();
+
+  const add = (a = 3, b = 4) => {
+      return a + b
+  }
+
+  add();
